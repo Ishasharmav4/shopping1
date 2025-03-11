9c1c5a6d1d94d612f682560cc5dda2f068c00e1c
ishasharma <ishasharmav4@gmail.com>
2025-03-11 14:56:29 +0530

    09-create-product-details-page

--- shopping/lib/constants.ts
+++ shopping/lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME=process.env.NEXT_PUBLIC_APP_NAME || 'ShopAmaze'
 export const APP_SLOGAN=process.env.NEXT_PUBLIC_APP_SLOGAN ||'Spend less,enjoy more.'
 export const APP_DESCRIPTION=process.env.NEXT_PUBILC_APP_DESCRIPTION || 'An online shopping platform'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
