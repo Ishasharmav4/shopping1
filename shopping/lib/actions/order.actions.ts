e5c297c53cf1716cbadc563a53ed7e2dfe3b15be
ishasharma <ishasharmav4@gmail.com>
2025-03-18 11:30:47 +0530

    19-pay-order-by-paypal

--- shopping/lib/actions/order.actions.ts
+++ shopping/lib/actions/order.actions.ts
@@ -4,9 +4,12 @@ import { formatError, round2 } from '../utils'
 
 import { auth } from '@/auth'
 import { connectToDatabase } from '../db'
-import Order from '../db/models/order.model'
+import Order, { IOrder } from '../db/models/order.model'
 import { OrderInputSchema } from '../validator'
 import { AVAILABLE_DELIVERY_DATES } from '../constants'
+import { paypal } from '../paypal'
+import { sendPurchaseReceipt } from '@/emails'
+import { revalidatePath } from 'next/cache'
 export const createOrder = async (clientSideCart: Cart) => {
   try {
     await connectToDatabase()
@@ -26,6 +29,73 @@ export const createOrder = async (clientSideCart: Cart) => {
     return { success: false, message: formatError(error) }
   }
 }
+export async function getOrderById(orderId:string):Promise<IOrder>{
+  await connectToDatabase()
+  const order =await Order.findById(orderId)
+  return JSON.parse(JSON.stringify(order))
+}
+export async function createPayPalOrder(orderId: string) {
+  await connectToDatabase()
+  try {
+    const order = await Order.findById(orderId)
+    if (order) {
+      const paypalOrder = await paypal.createOrder(order.totalPrice)
+      order.paymentResult = {
+        id: paypalOrder.id,
+        email_address: '',
+        status: '',
+        pricePaid: '0',
+      }
+      await order.save()
+      return {
+        success: true,
+        message: 'PayPal order created successfully',
+        data: paypalOrder.id,
+      }
+    } else {
+      throw new Error('Order not found')
+    }
+  } catch (err) {
+    return { success: false, message: formatError(err) }
+  }
+}
+export async function approvePayPalOrder(
+  orderId: string,
+  data: { orderID: string }
+) {
+  await connectToDatabase()
+  try {
+    const order = await Order.findById(orderId).populate('user', 'email')
+    if (!order) throw new Error('Order not found')
+
+    const captureData = await paypal.capturePayment(data.orderID)
+    if (
+      !captureData ||
+      captureData.id !== order.paymentResult?.id ||
+      captureData.status !== 'COMPLETED'
+    )
+      throw new Error('Error in paypal payment')
+    order.isPaid = true
+    order.paidAt = new Date()
+    order.paymentResult = {
+      id: captureData.id,
+      status: captureData.status,
+      email_address: captureData.payer.email_address,
+      pricePaid:
+        captureData.purchase_units[0]?.payments?.captures[0]?.amount?.value,
+    }
+    await order.save()
+    await sendPurchaseReceipt({ order })
+    revalidatePath(`/account/orders/${orderId}`)
+    return {
+      success: true,
+      message: 'Your order has been successfully paid by PayPal',
+    }
+  } catch (err) {
+    return { success: false, message: formatError(err) }
+  }
+}
+
 export const createOrderFromCart = async (
   clientSideCart: Cart,
   userId: string
@@ -52,6 +122,7 @@ export const createOrderFromCart = async (
   })
   return await Order.create(order)
 }
+
 export const calcDeliveryDateAndPrice = async ({
   items,
   shippingAddress,
