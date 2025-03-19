3042841fa733c33108947aa194da85bc7bf790ca
ishasharma <ishasharmav4@gmail.com>
2025-03-19 10:05:19 +0530

    25-create-search-page

--- shopping/components/shared/header/search.tsx
+++ shopping/components/shared/header/search.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { getAllCategories } from "@/lib/actions/product.actions"
 import { APP_NAME } from "@/lib/constants"
 
 import { SearchIcon } from "lucide-react"
@@ -7,8 +8,10 @@ import { SearchIcon } from "lucide-react"
 
 
 
-const categories=['men','women','kids','accessories']
+
     export default async function Search(){
+        const categories = await getAllCategories()
+
         return(
             <form action='/search' method='GET' className='flex items-stretch h-10'>
                 <Select name='category'>
@@ -25,7 +28,7 @@ const categories=['men','women','kids','accessories']
                     </SelectContent>                   
                 </Select>
                 <Input className='flex-1 rounded-none dark:border-gray-200 bg-gray-100 text-black text-base h-full' placeholder={`Search Site ${APP_NAME}`}name='q' type='search'/>
-                <button type='submit' className='bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2'>
+                <button type='submit' className='bg-primary text-primary-foreground text-blue  rounded-s-none rounded-e-md h-full px-3 py-2'>
                 <SearchIcon className='w-6 h-6' />
                 </button>
             </form>
